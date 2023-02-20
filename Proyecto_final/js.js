1e3ab51f4ad57fc1c53e4baeb88ac11cd86975fe
Bernardo <bernardo_ruizesquide@hotmail.com>
2023-02-19 23:18:50 -0300

    mas ejemplos

--- Proyecto_final/js.js
+++ Proyecto_final/js.js
@@ -11,14 +11,52 @@ async function api() {
     document.getElementById("imagenCara").src = cv.results[0].picture.thumbnail
     document.getElementById("nombre").innerHTML = cv.results[0].name.first
     document.getElementById("apellido").innerHTML = cv.results[0].name.last
-    document.getElementById("edad").innerHTML = cv.results[0].dob.age
+    document.getElementById("edad").innerHTML = cv.results[0].dob.age+" años"
+    document.getElementById("per").innerHTML = "Ciudad de Residencia: " + cv.results[0].location.city +
+     " ☺ País de residencia: " + cv.results[0].location.country + " ☺ Uso horario para recibir llamadas: "
+      + cv.results[0].location.timezone.description
 
-    
 
 }
 /*document.getElementById("cuerpo").innerHTML = */
 
 api()
 
+
+document.getElementById("breve").addEventListener('mouseover', function () {
+    document.getElementById("des").style.display = "inline";
+    document.getElementById("per").style.display = "none";
+    document.getElementById("lab").style.display = "none";
+});
+
+document.getElementById("datos").addEventListener('mouseover', function () {
+    document.getElementById("des").style.display = "none";
+    document.getElementById("per").style.display = "inline";
+    document.getElementById("lab").style.display = "none";
+});
+
+document.getElementById("meta").addEventListener('mouseover', function () {
+    document.getElementById("des").style.display = "none";
+    document.getElementById("per").style.display = "none";
+    document.getElementById("lab").style.display = "inline";
+});
+
+document.getElementById("expBoton").addEventListener('click', function () {
+    document.getElementById("exp").style.display = "inline";
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("vid").style.display = "none";
+});
+
+document.getElementById("eduBoton").addEventListener('click', function () {
+    document.getElementById("exp").style.display = "none";
+    document.getElementById("edu").style.display = "inline";
+    document.getElementById("vid").style.display = "none";
+});
+
+document.getElementById("vidBoton").addEventListener('click', function () {
+    document.getElementById("exp").style.display = "none";
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("vid").style.display = "inline";
+});
 
 
